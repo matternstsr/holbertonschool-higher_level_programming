89a0c3d955bb3e2c23e2e32edbfc8dfc8f64225a
matternstsr <matternstsr@gmail.com>
2023-09-05 12:44:24 -0500

    need_to_have_2_lines_for_betty_in_py_i_guess

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,7 @@ def random_number():
     number = random.randint(-10000, 10000)
     return number
 
+
 def get_last_digit(number):
     if number >= 0:
         return number % 10
@@ -14,6 +15,7 @@ def get_last_digit(number):
         else:
             return 0
 
+
 def print_last_digit(last_digit):
     print('Last digit of {:d} is {:d}'.format(number, last_digit), end=" ")
     if last_digit > 5:
@@ -22,6 +24,7 @@ def print_last_digit(last_digit):
         print('{}'.format('and is 0'))
     elif last_digit < 6 and last_digit != 0:
         print('{}'.format('and is less than 6 and not 0'))
+
 
 if __name__ == '__main__':
     number = random_number()
