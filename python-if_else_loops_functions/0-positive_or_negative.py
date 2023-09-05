b89d14eb0bdbf18b0c52af9ccfc7cf0a37a0830d
matternstsr <matternstsr@gmail.com>
2023-09-05 12:06:31 -0500

    numbier_is_not_right

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number < 0:
-    print("{:d} is negative".format(numbier))
+    print("{:d} is negative".format(number))
 elif number > 0:
     print("{:d} is positive".format(number))
 else:
