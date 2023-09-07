03c2033efb253cd5aaa88304d083ef8daeab089f
matternstsr <matternstsr@gmail.com>
2023-09-06 21:22:34 -0500

    oops

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 
-def print_uniq_combos():
+def print_unique_combos():
     for a in range(10):
-        for b in range(ai + 1, 10):
+        for b in range(a + 1, 10):
             if a == 8 and b == 9:
                 print('{:d}{:d}'.format(a, b))
             else:
@@ -11,4 +11,4 @@ def print_uniq_combos():
 
 
 if __name__ == '__main__':
-    print_uniq_combos()
+    print_unique_combos()
