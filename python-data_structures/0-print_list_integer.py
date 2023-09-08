61bb747563398a0816027166db851198b87e3654
matternstsr <matternstsr@gmail.com>
2023-09-08 10:05:20 -0500

    traivshelp

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    if type(my_list) == list and len(my_list) > 0:
+def print_list_inteerger(my_list=[]):
+    if type(my_list) == list and my list is not None:
         for idx in my_list:
             print('{:d}'.format(idx))
