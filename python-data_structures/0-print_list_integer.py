7b3c90e0a2439bac4a5fc5b03d326d31085f9a08
matternstsr <matternstsr@gmail.com>
2023-09-08 10:32:15 -0500

    shouldnt matter but tying it switched

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_interger(my_list=[]):
-    if type(my_list) == list and my_list is not None:
+    if my_list is not None and type(my_list) == list:
         for idx in my_list:
             print("{:d}".format(idx))
