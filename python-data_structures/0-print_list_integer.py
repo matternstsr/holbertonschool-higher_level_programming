caf89ed7cd6cf5d0371428a510eaa1994293fc66
matternstsr <matternstsr@gmail.com>
2023-09-08 09:58:48 -0500

    checking to make sure its a lista nd not empty

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for idx in my_list:
-        print('{:d}'.format(idx))
+    if type(my_list) == list and len(my_list) > 0:
+        for idx in my_list:
+            print('{:d}'.format(idx))
