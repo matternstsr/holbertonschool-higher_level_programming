a998d99440d35fb2b02256472d52e84fea046e9c
matternstsr <matternstsr@gmail.com>
2023-09-12 16:19:14 -0500

    canges to 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 
 def max_integer(my_list=[]):
-    max_int = 0
+    max_value = 0
     if len(my_list) == 0:
         return None
     for index, element in enumerate(my_list):
-        if index == 0 or element > max_int:
-            max_int = element
-    return max_int
+        if index == 0 or element > max_value:
+            max_value = element
+    return max_value
