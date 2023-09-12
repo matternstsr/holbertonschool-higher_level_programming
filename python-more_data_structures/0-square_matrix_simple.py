932ae96a2f2a7217268cdc68bfb767b47017884a
matternstsr <matternstsr@gmail.com>
2023-09-12 16:50:41 -0500

    internet is in and out :(

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print('{:d}'.format(i))
+def square_matrix_simple(matrix=[]):
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
