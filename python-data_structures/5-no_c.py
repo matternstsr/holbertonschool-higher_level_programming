af40ec624b97a7f916e3ebfcedcb41f901461e2b
matternstsr <matternstsr@gmail.com>
2023-09-12 11:43:57 -0500

    think indents were off and had space in var

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,9 +2,9 @@
 
 
 def no_c(my_string):
-   my_new_list = list(my_string)
+    my_new_list = list(my_string)
     my_new_string = ''
-    for char in my_ new_list:
+    for char in my_new_list:
         if character != 'c' and character != 'C':
             my_new_string += character
-    return my_new_string
+            return my_new_string
