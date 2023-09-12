b0c31464e8f381f3060b9b3544e14c3252bfb02b
matternstsr <matternstsr@gmail.com>
2023-09-12 12:03:30 -0500

    first 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) != 0:
+        numrows = len(matrix)
+        numcolumns = len(matrix[0])
+        for rows in range(numrows):
+            for columns in range(numcolumns):
+                if columns == numcolumns - 1:
+                    print('{:d}'.format(matrix[rows][columns]))
+                else:
+                    print('{:d} '.format(matrix[rows][ccolumns])
+    else:
+        print[]
