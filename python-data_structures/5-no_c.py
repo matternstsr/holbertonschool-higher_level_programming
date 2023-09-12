d0a93e3699e034f7d82f94ef5cc17fcf7498b101
matternstsr <matternstsr@gmail.com>
2023-09-12 11:40:07 -0500

    trying 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+   my_new_list = list(my_string)
+    my_new_string = ''
+    for char in my_ new_list:
+        if character != 'c' and character != 'C':
+            my_new_string += chararacter
+    return my_new_string
