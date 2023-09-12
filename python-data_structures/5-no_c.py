233345890a29972ef14764939c60ffecdb6d87fe
matternstsr <matternstsr@gmail.com>
2023-09-12 11:41:09 -0500

    oops chararacter is character

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -6,5 +6,5 @@ def no_c(my_string):
     my_new_string = ''
     for char in my_ new_list:
         if character != 'c' and character != 'C':
-            my_new_string += chararacter
+            my_new_string += character
     return my_new_string
