5abad69ff5baa7c546625c2ae08c783eecf9aea7
matternstsr <matternstsr@gmail.com>
2023-09-12 20:29:49 -0500

    see if the interenet lets me.

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is not None:
+        keysorted= sorted(a_dictionary.keys())
+        for key in keysorted:
+            print("{:s}: {}".format(k, a_dictionary[key]))
