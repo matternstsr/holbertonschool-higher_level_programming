2a93e6b79cc1dc8125cdabe75ebf3c3f04c7a29d
matternstsr <matternstsr@gmail.com>
2023-09-12 22:07:42 -0500

    doesnt incrment

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for idx in range(x):
         try:
             print('{:d}'.format(my_list[idx]), end='')
+            cnt += 1
         except ValueError:
             pass
         except TypeError:
