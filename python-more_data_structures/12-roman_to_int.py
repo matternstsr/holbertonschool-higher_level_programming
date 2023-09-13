075b2444acd79b03823f22c80dab4f580f64eef3
matternstsr <matternstsr@gmail.com>
2023-09-12 21:19:21 -0500

    so far 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if roman_string is not None and type(roman_string) == str:
+        rom_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
