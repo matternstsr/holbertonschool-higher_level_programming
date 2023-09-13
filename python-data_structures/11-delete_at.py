bb8c298df559e4f619d3da983c10032f88d6340d
matternstsr <matternstsr@gmail.com>
2023-09-12 21:29:48 -0500

    or not and

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], index=0):
-    if index < 0 and index >= len(my_list):
+    if index < 0 or index >= len(my_list):
         return my_list
     del my_list[index]
     return my_list
