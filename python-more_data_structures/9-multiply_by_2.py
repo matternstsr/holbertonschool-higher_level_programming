c24fb32646995fa1df67fb061626a7239a17a129
matternstsr <matternstsr@gmail.com>
2023-09-12 20:59:55 -0500

    making this too hard i think

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    if a_dictionary is not None:
-        created = dict(a_dictionary)
-        for key, value in created.items():
-            created[key] = value * 2
-            return created
+    return ({key: a_dictionary[key] * 2 for key in a_dictionary})
