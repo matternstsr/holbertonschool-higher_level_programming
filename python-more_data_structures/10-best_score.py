41094f6e33fb7cdbbc385e0433fc78de83627d28
matternstsr <matternstsr@gmail.com>
2023-09-12 21:02:22 -0500

    think i had that back wards

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,9 +4,9 @@
 def best_score(a_dictionary):
     if a_dictionary is not None:
         score = None
-        person = None
+        player = None
         for key in a_dictionary.keys():
-            if person None or a_dictionary[key] > person:
-                person = a_dictionary[key]
-                score = key
+            if score None or a_dictionary[key] > score:
+                score = a_dictionary[key]
+                player = key
         return score
