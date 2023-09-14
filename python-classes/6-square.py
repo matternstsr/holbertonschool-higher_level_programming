8139c8f1e774d8c1cf8de510ddb08739f68eb6c8
matternstsr <matternstsr@gmail.com>
2023-09-14 12:21:04 -0500

    new line at end

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -74,3 +74,4 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+            
