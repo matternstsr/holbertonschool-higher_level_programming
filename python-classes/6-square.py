2ecdf3b0a544e096ce56716880f4248bc076b407
matternstsr <matternstsr@gmail.com>
2023-09-14 12:34:59 -0500

    trying other

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,6 +6,7 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
 
@@ -13,13 +14,13 @@ class Square:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """set the size of the square."""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -31,27 +32,26 @@ class Square:
 
     @property
     def position(self):
-        """set the position of the square."""
-        return self.__position
+        """Get/set the current position of the square."""
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        msgerr = "position must be a tuple of 2 poisitive integers"
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(v, int) for v in value) or
-                any(v < 0 for v in value)):
-            raise TypeError(msgerr)
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Return the area of the square."""
-        return self.__size * self.__size
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Print square with the #. """
+        """Print the square with the # character."""
         if self.__size == 0:
-            print()
+            print("")
             return
 
         [print("") for i in range(0, self.__position[1])]
@@ -59,4 +59,3 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-            
