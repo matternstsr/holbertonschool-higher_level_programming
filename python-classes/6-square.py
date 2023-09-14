9d046bff1967be8dad4dbef7a0d0338884b5a86c
matternstsr <matternstsr@gmail.com>
2023-09-14 10:59:30 -0500

    had to add positioning

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,22 +5,13 @@
 
 
 class Square:
-    """Represent a square."""
-
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-            position (int, int): The position of the new square.
-        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -32,26 +23,25 @@ class Square:
 
     @property
     def position(self):
-        """Get/set the current position of the square."""
-        return (self.__position)
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(number, int) for number in value) or
-                not all(number >= 0 for number in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if (
+            not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(v, int) for v in value) or
+            any(v < 0 for v in value)
+        ):
+            raise TypeError("position must be a tuple of two positive integers")
         self.__position = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
-    
+        return self.__size * self.__size
+
     def my_print(self):
-        """Print the square with the # character."""
         if self.__size == 0:
-            print("")
+            print()
             return
 
         for i in range(0, self.__position[1]):
@@ -59,6 +49,21 @@ class Square:
         for i in range(0, self.__size):
             for j in range(0, self.__position[0]):
                 print(" ", end="")
-                for k in range(0, self.__size):
-                    print("#", end="")
-                    print("")
+            for k in range(0, self.__size):
+                print("#", end="")
+            print()
+
+    def display(self):
+        if self.__size == 0:
+            print()
+            return
+
+        pattern = [
+            [" " * self.__position[0] + "#" * self.__size]
+            for _ in range(self.__position[1])
+        ]
+
+        pattern.extend([" " * self.__position[0] + "#" * self.__size for _ in range(self.__size)])
+
+        print("\n".join(pattern))
+
