33d0825814eb0d55c96fd40c07a2fcdd412a3f72
matternstsr <matternstsr@gmail.com>
2023-09-14 08:31:28 -0500

    need to define them with documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-
+# 0-square.py
+# Matthew Ernst <6628@holbertonstudents.com>
+"""Define a class - Square."""
 class Square:
+    """Empty class representing a square."""
     pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size):
+        Args:
+            size (int): The size of the new square.
+        self.__size = size
