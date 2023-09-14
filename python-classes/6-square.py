3de2d6c7f3cacf1883e640f06f2c2552660bd9df
matternstsr <matternstsr@gmail.com>
2023-09-14 12:23:05 -0500

    oops py code style

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -54,21 +54,6 @@ class Square:
             print()
             return
 
-        for i in range(0, self.__position[1]):
-            print("")
-        for i in range(0, self.__size):
-            for j in range(0, self.__position[0]):
-                print(" ", end="")
-            for k in range(0, self.__size):
-                print("#", end="")
-            print()
-
-    def display(self):
-        """Print square with the # to the position. """
-        if self.__size == 0:
-            print()
-            return
-
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
