d171db2898c9434063fce489424381479301b20b
matternstsr <matternstsr@gmail.com>
2023-09-14 08:51:33 -0500

    1 def size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+# Matthew Ernst <6628@holbertonstudents.com>
+"""Define a class Square."""
+
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
+        """
         self.__size = size
+
