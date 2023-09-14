1b0c107185629ce8af167d686e38ae5fc9e50dab
matternstsr <matternstsr@gmail.com>
2023-09-14 09:28:06 -0500

    comments have to between the action and the statement

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,11 +14,11 @@ class Square:
             size (int): The size of the new square.
         """
         if not isinstance(size, int):
+            """Elvaluates if it is an (instance) int
+        and if NOT values true due to the not."""
             raise TypeError("size must be an integer")
         elif size < 0:
+            """checks to see if size is bigger than or equal to 0."""
             raise ValueError("size must be >= 0")
-        """Elvaluates if it is an (instance) int
-        and if NOT values true due to the not."""
-        """checks to see if size is bigger than or equal to 0."""
         self.__size = size
         """sets the size of the square ."""
