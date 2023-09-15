a7ab50608873e8e791293d4c3c9ca6df343afa99
matternstsr <matternstsr@gmail.com>
2023-09-15 12:52:14 -0500

    wrong documentation in function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 # 0-add_integer.tpy
 # Matthew Ernst <6628@holbertonstudents.com>
-"""Adds two integers: """
-
-"""Returns the sum of adding integers a and b.
-
-    Float casted to int before adding.
-
-    Raises:
-        TypeError: if a or b are not float or int
-"""
+""" function to add two integers """
 
 
 def add_integer(a, b=98):
-    """adds two inegers"""
+    """Returns the sum of integers a and b.
+
+    Float arguments will be casted to int before adding.
+
+    Raises:
+        TypeError: if a or b are not float or int
+    """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
