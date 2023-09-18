39e81d4473881a37fa3a97b8aa06aa617d031e76
matternstsr <matternstsr@gmail.com>
2023-09-18 08:55:09 -0500

    2-matrix_divided.py:16:1: W293 blank line contains whitespace

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -13,7 +13,7 @@ def matrix_divided(matrix, div):
 
     rowlen = len(matrix)
     collen = len(matrix[0])
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
@@ -21,8 +21,8 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
-            raise TypeError("Each row of the matrix must have the same size")
-    
+        raise TypeError("Each row of the matrix must have the same size")
+
     for row in matrix:
         for element in row:
             if type(element) != int and float:
