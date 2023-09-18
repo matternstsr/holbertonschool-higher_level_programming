30cd6b9d8e9b037eff64cd92f9ebaf11d9840300
matternstsr <matternstsr@gmail.com>
2023-09-18 09:26:57 -0500

    better 2 passed and 5 failed.

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,9 +5,8 @@
 def say_my_name(first_name, last_name=""):
     """Print first and last"""
 
-if type(first_name) is not str:
-        raise TypeError("first_name must be a string")
-elif type(last_name) is not str:
+    if type(first_name) is not str:
+         raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
-
-print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
