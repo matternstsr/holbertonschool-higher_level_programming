a627620d626c7b1c58d4fa5de3dabb4ecbe2e568
matternstsr <matternstsr@gmail.com>
2023-09-18 11:39:04 -0500

    so far

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,6 +3,21 @@
 
 
 def text_indentation(text):
-
-        if type(text) is not str:
+    
+    if type(text) is not str:
         raise TypeError("text must be a string")
+    
+    """prints adouble new line at points: """
+    sen = ""
+    i = 0
+    while a < len(text):
+        if text[a] in not "." and text[a] is not "?" and text[a] is not ":":
+            sen += text[a]
+        else:
+            sen += text[a]
+            print(sen)
+            print()
+            sen = ""
+            while a < (len(text) - 1) and text[a+1] == " ":
+                a += 1
+    print(sen, end="")
