9356a229fd4dbd4e05789addd68732d6926677c9
matternstsr <matternstsr@gmail.com>
2023-09-18 09:23:50 -0500

    trying the other way?

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,9 +5,9 @@
 def say_my_name(first_name, last_name=""):
     """Print first and last"""
 
-if type(last_name) != str:
+if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    elif type(first_name) != str:
+    elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
 print("My name is {:s} {:s}".format(first_name, last_name))
