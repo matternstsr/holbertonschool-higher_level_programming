e1a96dd21bdee76b47e0c9ae702caf717a68ca79
matternstsr <matternstsr@gmail.com>
2023-09-18 22:00:12 -0500

    objectives

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 # 6-max_integer_test.py
 # Matthew Ernst  <6628@holbertonstudents.com>
+#File is present - good 
+#All unittest passed
+#Test for “max at the end” exists
+#Test for “max at the beginning” exists
+#Test for “max in the middle” exists
+#Test for “one negative number in the list” exists
+#Test for “only negative numbers in the list” exists
+#Test for “list of one element” exists
+#Test for “list is empty” exists
 
 max_integer = __import__('6-max_integer').max_integer
