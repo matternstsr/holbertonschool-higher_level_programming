54c1cf22e0ccd2a123f7c0283b6fc56ef7028e25
matternstsr <matternstsr@gmail.com>
2023-09-19 13:05:32 -0500

    so far

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+# Matt Ernst
+""" class Rectangle that defines a rectangle0-rectangle.py  ". """
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        
