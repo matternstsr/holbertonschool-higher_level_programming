a0b9ceac87cf3c2593c7d233854d15aada27f492
matternstsr <matternstsr@gmail.com>
2023-09-19 13:10:17 -0500

    hmmph

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,3 +9,26 @@ class Rectangle:
         self.width = width
         self.height = height
         
+    @property
+    def width(self):
+        return self.width
+    
+    @property
+    def height(self):
+        return self.height
+    
+    @width.setter
+    def width(self, value):
+        if is not (value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.width = value
+
+     @height.setter
+    def height(self, value):
+        if is not (value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.height = value
