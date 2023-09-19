0ae122e5af7a2116fa81c572f69b40d5335ebcf0
matternstsr <matternstsr@gmail.com>
2023-09-19 14:43:47 -0500

    Rectangle.number_of_instances += 1???

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
         """Initialize new class rectangle"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
