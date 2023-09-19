b1fd6ecf5264ce0106e85c74f60435b76d08bec2
matternstsr <matternstsr@gmail.com>
2023-09-19 14:59:08 -0500

    added the print_symbol = "#"

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -7,6 +7,7 @@
 class Rectangle:
     """Define new class"""
     number_of_instances = 0
+    print_symbol = "#"
     
     def __init__(self, width=0, height=0):
         """Initialize new class rectangle"""
