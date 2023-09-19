f6514bd746321c0c213615b3848e1c026676b127
matternstsr <matternstsr@gmail.com>
2023-09-18 22:04:13 -0500

    outlines

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,5 +10,12 @@
 #Test for “only negative numbers in the list” exists
 #Test for “list of one element” exists
 #Test for “list is empty” exists
+#you will write unittests for the function def max_integer(list=[]):.
+#Your test file should be inside a folder tests
+#You have to use the unittest module
+#Your test file should be python files (extension: .py)
+#should be exec using command: python3 -m unittest tests.6-max_integer_test
+#All tests you make must be passable by the function below
+"""tests for 6-max_integer"""
 
 max_integer = __import__('6-max_integer').max_integer
