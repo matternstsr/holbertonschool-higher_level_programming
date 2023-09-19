8e2bbf73c02a89e2c912a9cb09bd4223fda29aa4
matternstsr <matternstsr@gmail.com>
2023-09-19 12:50:07 -0500

    trying 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Matt Ernst
+""" Defines a class "Rectangle". """
+
+class Rectangle:
+    """This is an EMPTY class to define Rectangle"""
+    """Pass due to empty - no args"""
+    pass
