337d772770d79bea9221ae7c0f7ea0671d5a037e
matternstsr <matternstsr@gmail.com>
2023-09-19 13:13:53 -0500

    dont get the language but didnt like the is

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     
     @width.setter
     def width(self, value):
-        if is not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -27,7 +27,7 @@ class Rectangle:
 
      @height.setter
     def height(self, value):
-        if is not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -38,5 +38,12 @@ class Rectangle:
     Rectangle = __import__('1-rectangle').Rectangle
   File "/home/matternstsr/holbertonschool-higher_level_programming/python-more_classes/1-rectangle.py", line 22
     if is not (value, int):
+       ^^
+SyntaxError: invalid syntax"""
+"""Traceback (most recent call last):
+  File "/home/matternstsr/holbertonschool-higher_level_programming/python-more_classes/./1-main.py", line 2, in <module>
+    Rectangle = __import__('1-rectangle').Rectangle
+  File "/home/matternstsr/holbertonschool-higher_level_programming/python-more_classes/1-rectangle.py", line 22
+    if is not isinstance(value, int):
        ^^
 SyntaxError: invalid syntax"""
