36b34311eeae0d7a308cf8806b5f6aa0078ffaf9
matternstsr <matternstsr@gmail.com>
2023-09-19 15:04:37 -0500

    helps to add it to the code desides declaring it lol

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
         pattern = []
         for i in range(self.__height):
             for j in range(self.__width):
-                pattern.append('#')
+                pattern.append(print_symbol)
             if i is not self.__height - 1:
                 pattern.append("\n")
         return ("".join(pattern))
