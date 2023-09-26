e0e81bd7de73e942bc4688ecf08db62d98131acf
matternstsr <matternstsr@gmail.com>
2023-09-26 18:14:37 -0500

    didnt have tot do that before?

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -25,13 +25,13 @@ import json
 
 """import json???"""
 
-
-save_json = __import__('7-save_to_json_file').save_to_json_file
-get_json = __import__('8-load_from_json_file').load_from_json_file
-
-try:
-    items = get_json("add_item.json")
-except FileNotFoundError:
-    items = []
-items.extend(sys.argv[1:])
-save_json(items, "add_item.json")
+if __name__ == "__main__":
+    save_json = __import__('7-save_to_json_file').save_to_json_file
+    get_json = __import__('8-load_from_json_file').load_from_json_file
+    
+    try:
+        items = get_json("add_item.json")
+    except FileNotFoundError:
+        items = []
+        items.extend(sys.argv[1:])
+        save_json(items, "add_item.json")
