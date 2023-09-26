1175aeee3d1c40a43059c7a4a042c659cf90ce9c
matternstsr <matternstsr@gmail.com>
2023-09-26 16:10:18 -0500

    try that

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -12,6 +12,6 @@ import json
 
 
 def load_from_json_file(filename):
-    """ creates an Object from a “JSON file”"""
-    with open(filename, mode="w", encoding="utf-8") as doittoit:
+    """ creates an Object from a JSON file"""
+    with open(filename) as doittoit:
         return json.load(doittoit)
