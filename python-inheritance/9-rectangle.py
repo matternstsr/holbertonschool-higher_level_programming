82f944839540c6aed22fb4ed1e101cff9f348a9d
matternstsr <matternstsr@gmail.com>
2023-09-25 22:20:51 -0500

    send the 8 area

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# 8-rectangle.py
+# Matthew Ernst 6628@holbertonstudents.com
+"""makes an empty class BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Empty class Rectangle"""
+    def __init__(self, width, height):
+        """Initilizing the Rectangle"""
+        """Need to bring in the integer_validator with new added properties"""
+        """Using integer_validator can ensure positive #s"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+    
+    def area(self):
+        """Finding area of rectangle"""
+        return self.__width * self.__height
+    
+    
+    
