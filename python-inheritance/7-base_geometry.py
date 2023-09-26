f814bd8d92b6f0c12fe034b4af01d857dd94e5e2
matternstsr <matternstsr@gmail.com>
2023-09-25 19:56:14 -0500

    pushing pass

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,6 +11,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
     
     def integer_validator(self, name, value):
-        """that validates value"""
-        if value is not type(int):
-            raise Exception (<name> "must be an integer")
+        pass
+#        """that validates value"""
+#        if value is not type(int):
+#            raise Exception (<name> "must be an integer")
