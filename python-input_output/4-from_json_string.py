69d8616aa0319c9d89d59efe43d50fefb6b3d14e
matternstsr <matternstsr@gmail.com>
2023-09-26 15:15:31 -0500

    loads a sjon

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,4 +8,4 @@ import json
 def from_json_string(my_str):
     """ returns an object (Python data structure)
     represented by a JSON string"""
-    return (json.dumps(my_str))
+    return (json.loads(my_str))
