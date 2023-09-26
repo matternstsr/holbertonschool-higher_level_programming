71622a274a87805044c1c7d6eedcc05065dc3260
matternstsr <matternstsr@gmail.com>
2023-09-26 16:03:16 -0500

    dump?

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -15,4 +15,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """ Saves sjon to specified filename"""
     with open(filename, mode="w", encoding="utf-8") as doittoit:
-        return json.dumps(my_obj, doittoit)
+        return json.dump(my_obj, doittoit)
