e4969a3c91fffd622b7a15f243c5ff2c1ea210fa
matternstsr <matternstsr@gmail.com>
2023-09-25 19:24:41 -0500

    hmm seperate it ....

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@
 
 def inherits_from(obj, a_class):
     """Sub class to sub class"""
-    if type(obj) != a_class and issubclass(type(obj), a_class):
-        return False
-    else:
-        return True
+    if issubclass(type(obj), a_class):
+        if type(obj) != a_class:
+            return True
+    return False
