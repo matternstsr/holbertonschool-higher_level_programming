f9a7ebdec7bf3a7eada6a1503bd4f45b6ab95d29
matternstsr <matternstsr@gmail.com>
2023-09-25 22:56:43 -0500

    changed and cleaned up due to inherits

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -3,34 +3,7 @@
 # Matthew Ernst 6628@holbertonstudents.com
 """makes an empty class BaseGeometry"""
 
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """ Empty class Rectangle"""
-    def __init__(self, width, height):
-        """Initilizing the Rectangle"""
-        """Need to bring in the integer_validator with new added properties"""
-        """Using integer_validator can ensure positive #s"""
-
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """Finding area of rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """print() should print, and str() should return
-        the following rectangle description: [Rectangle] <width>/<height>"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    
-    def __init__(self, size):
-        """class Square that inherits from Rectangle"""
-        return(self.__width * self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """class Square that inherits Rectangle"""
