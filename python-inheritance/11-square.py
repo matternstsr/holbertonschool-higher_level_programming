79ef5775967ad61ab292790fbab7cc3ca1fd50d9
matternstsr <matternstsr@gmail.com>
2023-09-25 23:13:03 -0500

    those wsp get me every time

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -15,7 +15,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-        
+
     def __str__(self):
         """It's a square now"""
         return '[Square] {}/{}'.format(self.__size, self.__size)
