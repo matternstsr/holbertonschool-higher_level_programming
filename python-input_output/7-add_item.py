2d38fcf04a16a17ce75dbfaed8474973345c8109
matternstsr <matternstsr@gmail.com>
2023-09-26 18:48:07 -0500

    try that

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,12 @@ import sys
 import json
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    save_json = __import__('5-save_to_json_file').save_to_json_file
+    load_json = __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        new_list = load_from_json_file("add_item.json")
+        new_list = load_json("add_item.json")
     except FileNotFoundError:
         new_list = []
     new_list.extend(sys.argv[1:])
-    save_to_json_file(new_list, "add_item.json")
+    save_json(new_list, "add_item.json")
