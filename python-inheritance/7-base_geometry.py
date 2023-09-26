1f6362bf4ead8b823563849ed0f8b9993cae3970
matternstsr <matternstsr@gmail.com>
2023-09-25 20:28:56 -0500

    of coarse!!! TypeError  and ValueError

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """that validates value"""
         if type(value) is not int:
-            raise Exception("{} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise Exception("{} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
