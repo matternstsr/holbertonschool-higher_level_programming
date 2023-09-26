ecb40d7ab09f4cfc61b5d51ab39dc892366c2446
matternstsr <matternstsr@gmail.com>
2023-09-26 15:17:14 -0500

    loads json

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,4 +8,7 @@ import json
 def from_json_string(my_str):
     """ returns an object (Python data structure)
     represented by a JSON string"""
+    """the loads() method for parsing JSON strings.
+    Then, you can assign the parsed data to a variable if
+    you need too (sure its coming)"""
     return (json.loads(my_str))
