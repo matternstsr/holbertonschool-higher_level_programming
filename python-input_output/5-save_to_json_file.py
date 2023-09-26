e8118fa94b642003e0495c443a17ff1613b0df45
matternstsr <matternstsr@gmail.com>
2023-09-26 15:32:44 -0500

    sending 5 eventhough think not quite right

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 1-my_list.py
+# Matthew Ernst 6628@holbertonstudents.com
+"""Write a function that writes an Object to a text file, using a JSON
+representation:
+Prototype: def save_to_json_file(my_obj, filename):
+You must use the with statement
+You don’t need to manage exceptions if the object can’t be serialized.
+You don’t need to manage file permission exceptions.
+combining the read and write json"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Saves sjon to specified filename"""
+        """write a string to the file"""
+    with open(filename, mode="w", encoding="utf-8") as doittoit:
+        return doittoit.write(my_obj)
