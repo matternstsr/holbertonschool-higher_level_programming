a203f58d4d15bbe22b0c376a337a2fe9e34f395f
matternstsr <matternstsr@gmail.com>
2023-09-25 19:50:57 -0500

    trying 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 6-is_same_class.py
+# 7-is_same_class.py
 # Matthew Ernst 6628@holbertonstudents.com
 """makes an empty class BaseGeometry"""
 
@@ -9,3 +9,8 @@ class BaseGeometry:
     def area(self):
         """returns the area if implimented"""
         raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """that validates value"""
+        if value is not type(int):
+            raise Exception (<name> "must be an integer")
