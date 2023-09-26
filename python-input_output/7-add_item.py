8adc0ae291e84b99b2cca08cdea62516116d597d
matternstsr <matternstsr@gmail.com>
2023-09-26 18:38:33 -0500

    picky picky picky

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,14 +5,14 @@
 import sys
 import json
 
-
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = \
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
         __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    new_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    new_list = []
-new_list.extend(sys.argv[1:])
-save_to_json_file(new_list, "add_item.json")
+    try:
+        new_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        new_list = []
+    new_list.extend(sys.argv[1:])
+    save_to_json_file(new_list, "add_item.json")
