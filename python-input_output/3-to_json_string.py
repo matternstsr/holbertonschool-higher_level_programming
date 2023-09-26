631f80ee9cc49e249554f535b3cf5b823608458f
matternstsr <matternstsr@gmail.com>
2023-09-26 15:03:07 -0500

    json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1-my_list.py
+# Matthew Ernst 6628@holbertonstudents.com
+""" DReturns the JSON of the file """
+import json
+
+def to_json_string(my_obj):
+    """Returns the rep of the object"""
+    return (json.dumps(my_obj))
