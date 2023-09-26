09172d4842f367b87c29b6542cde69df5f6dcbcc
matternstsr <matternstsr@gmail.com>
2023-09-25 23:12:24 -0500

    gotta have the print statment to pring dummy

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -7,7 +7,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """class Square that inherits Rectangle"""
+    """square with equal height, equal width"""
 
     def __init__(self, size):
         """Initializes new Square"""
@@ -15,3 +15,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+        
+    def __str__(self):
+        """It's a square now"""
+        return '[Square] {}/{}'.format(self.__size, self.__size)
