d987cc9c999828fec00dc0b88887eee5ba520262
matternstsr <matternstsr@gmail.com>
2023-09-25 19:38:33 -0500

    oops changed 5 and didnt make new 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-# 5-is_same_class.py
+# 6-is_same_class.py
 # Matthew Ernst 6628@holbertonstudents.com
 """makes an empty class BaseGeometry"""
 
 
 class BaseGeometry:
     """ Empty class BaseGeometry"""
-pass
+
+def area(self):
+        """returns the area"""
+        raise Exception("area() is not implemented")
