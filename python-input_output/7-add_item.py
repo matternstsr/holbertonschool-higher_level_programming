82cea093acde3b0db9fc61e99616cfa79da87b66
matternstsr <matternstsr@gmail.com>
2023-09-26 16:59:55 -0500

    bash pusher

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,17 +9,27 @@ The list must be saved as a JSON representation in a file named add_item.json
 If the file doesn’t exist, it should be created
 You don’t need to manage file permissions / exceptions."""
 """remember! argv[] is the name!!!!! """
+"""items.extend(sys.argv[1:]): This code extends the items list with all the
+command-line arguments passed to the script, starting from the first argument
+(index 1) to the end of the list. It treats sys.argv[1:] as an iterable and
+adds all elements from that iterable to the items list. This is what you
+typically want when you want to add all command-line arguments.
+items.extend(sys.argv[1]): This code attempts to extend the items list with a
+single element, which is the first command-line argument (at index 1) as a
+whole. This will not work as intended because sys.argv[1] is a single string,
+not an iterable of strings. It would result in a TypeError because you cannot
+directly extend a list with a single string"""
 
 import sys
 """import json???"""
 
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-get_json = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 try:
-    items = get_json('add_item.json')
+    items = load_from_json_file('add_item.json')
 except FileNotFoundError:
     items = []
-    items.extend(sys.argv[1])
-    save_json(items, 'add_item.json')
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, 'add_item.json')
