5d2ffeef834af839e237e432e9e5c3d618a7e45f
matternstsr <matternstsr@gmail.com>
2023-09-25 21:16:50 -0500

    trying to bring in validater using new args

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,3 +11,10 @@ class Rectangle(BaseGeometry):
     """ Empty class Rectangle"""
     def __init__(self, width, height):
         """Initilizing the Rectangle"""
+        """Need to bring in the integer_validator with new added properties"""
+        """Using integer_validator can ensure positive #s"""
+        
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
