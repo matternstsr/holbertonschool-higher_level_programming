41b533fa358f5ceadfd8076b7b452fc68708f51b
matternstsr <matternstsr@gmail.com>
2023-09-26 14:15:25 -0500

    send it

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
 # 1-my_list.py
 # Matthew Ernst 6628@holbertonstudents.com
-"""Write a function that reads a text file (UTF8)
-(Unicode Transformation Format - 8 bits) and prints it to stdout:
-Prototype: def read_file(filename=""):
+"""Write a function that writes a string to a text file (UTF8) and
+returns the number of characters written:
+Prototype: def write_file(filename="", text=""):
 You must use the with statement
-You don’t need to manage file permission or file doesn't exist exceptions.
+You don’t need to manage file permission exceptions.
+Your function should create the file if doesn’t exist.
+Your function should overwrite the content of the file if it already exists.
 You are not allowed to import any module"""
 
-"""The with statement in Python is used to wrap the execution of a block of
-code with methods defined by a context manager. In the case of file I/O
-operations, it provides a way to ensure that a file is closed when the block
-inside the with statement is exited, regardless of how the block is exited .
-
-The with statement is used to open the file
-specified by the filename parameter and read its contents using the utf-8
-encoding. The with statement automatically closes the file after its
-contents have been read .
-
-You don’t need to manage file permission or file doesn’t exist exceptions
-because the open() function will raise an exception if it fails to open the
-file ."""
-"""Open file and return a stream. Raise OSError upon failure.
-
-file is either a text or byte string giving the name (and the path if the
-file isn't in the current working directory) of the file to be opened or an
-integer file descriptor of the file to be wrapped. (If a file descriptor is
-given, it is closed when the returned I/O object is closed, unless closefd
-is set to False.)"""
 
 
 def read_file(filename=""):
