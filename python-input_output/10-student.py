8dfc2f18f14c2b6d1153690ed3022d30a8d1237f
matternstsr <matternstsr@gmail.com>
2023-09-26 20:23:05 -0500

    see what that does

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -24,3 +24,7 @@ class Student:
     def to_json(self):
         """Returns the dictionary student"""
         return (self.__dict__)
+
+    if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+    return self.__dict__
