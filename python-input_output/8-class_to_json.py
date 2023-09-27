2462c535fb07f7283f0ac81594571f3eeffe7cff
matternstsr <matternstsr@gmail.com>
2023-09-26 19:22:41 -0500

    send it

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# 7-save_to_json_file.py
+# 8-class_to_json.py
 # Matthew Ernst 6628@holbertonstudents.com
-
+"""comment"""
 
 def class_to_json(obj):
     """Serialize class attributes to dictionary
     obj (object): object to be serialized"""
-    return obj.__dict__
+    return (obj.__dict__)
