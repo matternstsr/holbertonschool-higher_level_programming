29f7816c574b1ca9d984cb3bde6c363a0fa6db0b
matternstsr <matternstsr@gmail.com>
2023-09-26 20:39:01 -0500

    the lines drive me nuts

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
-# 8-class_to_json.py
+# 10-class_to_json.py
 # Matthew Ernst 6628@holbertonstudents.com
-"""Write a class Student that defines a student by:
+"""Write a class Student that defines a student by: (based on 9-student.py)
 Public instance attributes:
 first_name
 last_name
 age
 Instantiation with first_name, last_name and age:
 def __init__(self, first_name, last_name, age):
-Public method def to_json(self): that retrieves a dictionary
-representation of a Student instance (same as 8-class_to_json.py)
+Public method def to_json(self, attrs=None):
+that retrieves a dictionary representation of a Student instance
+(same as 8-class_to_json.py):
+If attrs is a list of strings, only attribute names contained
+in this list must be retrieved.
+Otherwise, all attributes must be retrieved
 You are not allowed to import any module"""
+import json
 
 
 class Student:
     """its a student"""
+
     def __init__(self, first_name, last_name, age):
         """initalize a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Returns the dictionary student"""
-        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+
+def to_json(self, attrs=None):
+
+    """sort if there is a list and dont if not"""
+    if attrs is None:
+        return json.dumps(self.__dict__)
+    else:
+        return json.dumps({attr: getattr(self, attr)
+                           for attr in attrs if hasattr(self, attr)})
