20fc54f6ca0e2fab189d1604281bc036c28d46c4
matternstsr <matternstsr@gmail.com>
2023-09-26 19:43:18 -0500

    thats what i get for skipping pycodestyle

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -16,11 +16,11 @@ You are not allowed to import any module"""
 class Student:
     """Serialize class attributes to dictionary
     obj (object): object to be serialized"""
-    def __init__(self, first_name, last_name,age):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
         def to_json(self):
-           """Returns the dictionary rep of a student"""
-           return (obj.__dict__)
+            """Returns the dictionary rep of a student"""
+            return (obj.__dict__)
