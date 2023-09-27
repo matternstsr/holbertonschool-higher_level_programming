d333b510a68b2516d4d698ae40003a41b18a46b0
matternstsr <matternstsr@gmail.com>
2023-09-26 22:32:21 -0500

    object?

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -4,12 +4,12 @@
 """"""
 
 
-class Student(object):
+class Student:
     """Student class defined"""
 
     def __init__(self, first_name, last_name, age):
         """attribs defined for the class"""
-
+        
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
