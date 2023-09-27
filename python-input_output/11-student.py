41fae419a582b1ca5632b87165fee656fdf3c1b5
matternstsr <matternstsr@gmail.com>
2023-09-26 22:44:54 -0500

    try that. sorting too much

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -3,33 +3,25 @@
 # Matthew Ernst 6628@holbertonstudents.com
 """"""
 
-def class_to_json(obj):
-        return obj.__dict__
 
 class Student:
     """Student class defined"""
 
     def __init__(self, first_name, last_name, age):
         """attribs defined for the class"""
-        
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """all in list or all"""
-        obdict = class_to_json(self)
-        if not attrs:
-            return obdict
-        else:
-            fdict = {}
-            for kvp, value in obdict.items():
-                if kvp in attrs:
-                    fdict[kvp] = value
-            return fdict
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
+        """Load all attributes of the student and sort key value pairs."""
         for kvp, value in json.items():
-            if hasattr(self, kvp):
-                setattr(self, kvp, value)
+            setattr(self, kvp, value)
