198132055f7ac09708acb9f120e04d146a706053
matternstsr <matternstsr@gmail.com>
2023-09-26 19:19:45 -0500

    what is wrong

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,7 +2,8 @@
 # 7-save_to_json_file.py
 # Matthew Ernst 6628@holbertonstudents.com
 
+
 def class_to_json(obj):
-    """Serialize class attributes to dictionary"""
-    """obj (object): object to be serialized"""
+    """Serialize class attributes to dictionary
+    obj (object): object to be serialized"""
     return (obj.__dict__)
