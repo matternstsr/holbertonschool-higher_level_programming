ac13e8e1158fafc64c514e2f9512a8833d75dd3d
matternstsr <matternstsr@gmail.com>
2023-09-28 17:06:52 -0500

    like that???

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,6 +18,23 @@ class Rectangle(Base):
         self.__y = y
         """self.id = id old dont work"""
         super().__init__(id)
+    
+        if type(width) is not int:
+            raise TypeError("width" + self.typeer)
+        if width <= 0:
+            raise ValueError("width" + self.valer)
+        if type(height) is not int:
+            raise TypeError("height" + self.typeer)
+        if height <= 0:
+            raise ValueError("height" + self.valer)
+        if type(x) is not int:
+            raise TypeError("x" + self.typeer)
+        if x <= 0:
+            raise ValueError("x" + self.valer)
+        if type(y) is not int:
+            raise TypeError("y" + self.typeer)
+        if y <= 0:
+            raise ValueError("y" + self.valer)
 
     @property
     def width(self):
