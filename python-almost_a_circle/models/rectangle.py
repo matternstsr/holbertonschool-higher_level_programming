4ee24359a3549eb40cc6b017e89350fa6cb440da
matternstsr <matternstsr@gmail.com>
2023-09-28 16:27:13 -0500

    renamed old file and made new maybe that was it?

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,15 +7,13 @@ from models.base import Base
 
 class Rectangle(Base):
     """New Class Base"""
-    __typeer = " must be an integer"
-    __valer = " must be > 0"
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes Base Class with new attributes"""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         """self.id = id"""
         super().__init__(id)
 
@@ -25,27 +23,27 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """Setter for Width"""
-        if type(width) is not int:
-            raise TypeError("width" + self.__typeer) 
-        if width <= 0:
-            raise ValueError("width" + self.__valer)
-        self.__width = width
-        
+        if type(value) is not int:
+            raise TypeError("width must be an integer") 
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
     @property
     def height(self):
         """Getter for height"""
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """Setter for height"""
-        if type(height) is not int:
-            raise TypeError("height" + self.__typeer) 
-        if height <= 0:
-            raise ValueError("height" + self.__valer)
-        self.__height = height
+        if type(value) is not int:
+            raise TypeError("height must be an integer") 
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
@@ -53,13 +51,13 @@ class Rectangle(Base):
         return self.__x
 
     @x.setter
-    def x(self, x):
+    def x(self, value):
         """Setter for x"""
-        if type(x) is not int:
-            raise TypeError("x" + self.__typeer) 
-        if x <= 0:
-            raise ValueError("x" + self.__valer)
-        self.__x = x
+        if type(value) is not int:
+            raise TypeError("x must be an integer") 
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
@@ -67,13 +65,13 @@ class Rectangle(Base):
         return self.__y
 
     @y.setter
-    def y(self, y):
+    def y(self, value):
         """Setter for y"""
-        if type(y) is not int:
-            raise TypeError("y" + self.__typeer) 
-        if y <= 0:
-            raise ValueError("y" + self.__valer)
-        self.__y = y
+        if type(value) is not int:
+            raise TypeError("y must be an integer") 
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
     @property
     def id(self):
@@ -81,10 +79,10 @@ class Rectangle(Base):
         return self.__id
 
     @id.setter
-    def id(self, id):
+    def id(self, value):
         """Setter for id"""
-        if type(id) is not int:
-            raise TypeError("id" + self.__typeer) 
-        if id <= 0:
-            raise ValueError("id" + self.__valer)
-        self.__id = id
+        if type(value) is not int:
+            raise TypeError("id must be an integer") 
+        if value <= 0:
+            raise ValueError("id must be > 0")
+        self.__id = value
