fd831f154e8e04482f13ca3ada0dc6f5704d3fd8
matternstsr <matternstsr@gmail.com>
2023-09-28 09:42:23 -0500

    sending 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# base.py
+# Matthew Ernst 6628@holbertonstudents.com
+"""Makes a Base Class"""
+
+class Base:
+    """Class Base"""
+
+    def __init__(self, id=None):
+        """Initializes Base Class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
