4a66c9b1c6831d57a0bdd08b9362faca66c53f31
matternstsr <matternstsr@gmail.com>
2023-09-29 11:04:40 -0500

    display didnrt get moved to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,7 +23,42 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def display(self):
+        """Prints a representation of a Square using "#"."""
+        for _ in range(self.y):
+            print(f"{'' * self.x}")
+        for _ in range(self.height):
+            print(f"{' ' * self.x}{ '#' * self.width}")
+
     def __str__(self):
         """Returns the print() and str() representation of the Square."""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - "
                 f"{self.width}")
+
+    def update(self, *args, **kwargs):
+        """Returns the argument(s) selected or if no args then all"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+        if args is None:
+            return (f"[Square] ({self.id}) {self.x}/{self.y} - "
+                    f"{self.width}")
+        elif len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                if key == 'width':
+                    self.width = value
+                if key == 'height':
+                    self.height = value
+                if key == 'x':
+                    self.x = value
+                if key == 'y':
+                    self.y = value
