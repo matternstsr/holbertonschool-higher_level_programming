51ea7ed589615e40cd1f6375dfca1b0e0ef49839
matternstsr <matternstsr@gmail.com>
2023-09-29 02:41:04 -0500

    whew i think i have 8. that was hard and Im not sure i did it right still

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,6 +95,16 @@ class Rectangle(Base):
                 f"{self.width}/{self.height}")
 
     def update(self, *args):
-          return (f"[Rectangle] ({self.id}) {self.width}/{self.height} - "
-                f"{self.x}/{self.y}")
-  
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+        if args is None:
+            return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
+                f"{self.width}/{self.height}")
