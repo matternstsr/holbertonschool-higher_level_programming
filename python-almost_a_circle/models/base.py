f3a3023cd228abbe4b3648034a2859f959f0e1fb
matternstsr <matternstsr@gmail.com>
2023-10-03 09:58:32 -0500

    models/base.py:23:9: E117 over-indented

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,6 +20,6 @@ class Base:
 
 @staticmethod
 def to_json_string(list_dictionaries):
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
+    if list_dictionaries is None or len(list_dictionaries) == 0:
+        return "[]"
+    return json.dumps(list_dictionaries)
