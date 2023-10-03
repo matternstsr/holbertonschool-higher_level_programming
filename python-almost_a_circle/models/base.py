670cd5c2c8c91f4a3dadbedd4f466545fd8272e3
matternstsr <matternstsr@gmail.com>
2023-10-03 10:35:23 -0500

    @classmethod

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,7 +23,7 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
-    @staticmethod
+    @classmethod
     def save_to_file(cls, list_objs):
         filename = cls.__name__ + '.json'
 
