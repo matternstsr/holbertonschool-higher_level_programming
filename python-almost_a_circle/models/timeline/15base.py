43982b6c494341e4ff1999f2421c067f23d230f1
matternstsr <matternstsr@gmail.com>
2023-10-03 10:15:53 -0500

    def save_to_file(cls, list_objs):

--- python-almost_a_circle/models/timeline/15base.py
+++ python-almost_a_circle/models/timeline/15base.py
@@ -23,6 +23,11 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
-    @staticmethod
-    def save_to_file(cls, list_objs):
-        filename = cls.__name__ + '.json'
+"""15. Dictionary to JSON string
+JSON is one of the standard formats for sharing data representation.
+Update the class Base by adding the static method def 
+to_json_string(list_dictionaries): that returns the JSON string 
+representation of list_dictionaries:
+list_dictionaries is a list of dictionaries
+If list_dictionaries is None or empty, return the string: "[]"
+Otherwise, return the JSON string representation of list_dictionaries"""
