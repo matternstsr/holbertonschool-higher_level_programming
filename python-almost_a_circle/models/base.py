2e28d444961390d5109a6509500eef48dc1c70f5
matternstsr <matternstsr@gmail.com>
2023-10-03 10:08:32 -0500

    hmm try that

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,8 +18,8 @@ class Base:
             self.id = Base.__nb_objects
 
 
-@staticmethod
-def to_json_string(list_dictionaries):
-    if list_dictionaries is None or len(list_dictionaries) == 0:
-        return "[]"
-    return json.dumps(list_dictionaries)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
