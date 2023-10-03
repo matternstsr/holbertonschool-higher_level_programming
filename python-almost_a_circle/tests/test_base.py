0103b5788524037be34e0c0620daffaea81452c8
matternstsr <matternstsr@gmail.com>
2023-10-03 16:16:29 -0500

    test_isinstance out

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -10,24 +10,33 @@ from models.square import Square
 
 class TestBase(unittest.TestCase):
     """ This is a class that tests the base class"""
-
+"""
     def test_id_as_negative(self):
-        """
+        """"""
         Test for a negative Base Class id
-        """
+        """"""
         base_instance = Base(-100)
         self.assertEqual(base_instance.id, -100)
         base_instance = Base(-10)
         self.assertEqual(base_instance.id, -10)
 
+    def test_id_as_positive(self):
+        """"""
+        Test for a positive Base Class id
+        """"""
+        base_instance = Base(100)
+        self.assertEqual(base_instance.id, 100)
+        base_instance = Base(10)
+        self.assertEqual(base_instance.id, 10)
+
     def test_isinstance(self):
-        """
+        """"""
         Test a Base Class instance
-        """
+        """"""
         base_instance = Base()
         self.assertEqual(type(base_instance), Base)
         self.assertTrue(isinstance(base_instance, Base))
-
+"""
     def test_string_id(self):
         base_instance = Base('Monty Python')
         self.assertEqual(base_instance.id, 'Monty Python')
