444f9e17f9f55c438f080cd7691a07d92627dae2
matternstsr <matternstsr@gmail.com>
2023-10-03 13:52:50 -0500

    sending it eventhough it no workie

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -12,6 +12,16 @@ from models.square import Square
 class TestBase(unittest.TestCase):
     """ This is a class that tests the base class"""
 
+
+    def test_pep8_base(self):
+        """
+        Test that checks PEP8
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/base.py'])
+        self.assertEqual(check.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
     def test_id_as_negative(self):
         """
         Test for a negative Base Class id
@@ -29,12 +39,3 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base_instance.id, 100)
         base_instance = Base(10)
         self.assertEqual(base_instance.id, 10)
-
-    def test_pep8_base(self):
-        """
-        Test that checks PEP8
-        """
-        syntax = pep8.StyleGuide(quit=True)
-        check = syntax.check_files(['models/base.py'])
-        self.assertEqual(check.total_errors, 0,
-                         "Found code style errors (and warnings).")
