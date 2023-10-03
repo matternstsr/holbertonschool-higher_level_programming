1eee5126c0d0c25a4cd025a49ddc273b17bb39cf
matternstsr <matternstsr@gmail.com>
2023-10-03 18:42:33 -0500

    cant do the commas?

--- python-almost_a_circle/tests/tests_models/test_base.py
+++ python-almost_a_circle/tests/tests_models/test_base.py
@@ -1,6 +1,7 @@
 import unittest
-from models.base import Base, Rectangle, Square
-
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase(unittest.TestCase):
 
