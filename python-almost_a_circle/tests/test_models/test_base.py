e375c58d33b7f2a8e38945c131c4dd3768142bdb
matternstsr <matternstsr@gmail.com>
2023-10-03 13:13:43 -0500

    need imports?

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 def test_id_as_negative(self):
     """
