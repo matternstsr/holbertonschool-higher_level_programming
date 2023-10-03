8a22a7303fad4fb78895b0e78bad7ead40aae7da
matternstsr <matternstsr@gmail.com>
2023-10-03 13:08:33 -0500

    trying test neg

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def test_id_as_negative(self):
+    """
+    Test for a negative Base Class id
+    """
+    base_instance = Base(-91)
+    self.assertEqual(base_instance.id, -91)
+    base_instance = Base(-4)
+    self.assertEqual(base_instance.id, -4)
