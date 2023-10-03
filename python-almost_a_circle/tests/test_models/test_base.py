fdc5d8b6a617399a697d0eab92a1a5230b0bb55f
matternstsr <matternstsr@gmail.com>
2023-10-03 13:11:29 -0500

    trying positive which should be tru

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,7 +4,16 @@ def test_id_as_negative(self):
     """
     Test for a negative Base Class id
     """
-    base_instance = Base(-91)
-    self.assertEqual(base_instance.id, -91)
-    base_instance = Base(-4)
-    self.assertEqual(base_instance.id, -4)
+    base_instance = Base(-100)
+    self.assertEqual(base_instance.id, -100)
+    base_instance = Base(-10)
+    self.assertEqual(base_instance.id, -10)
+
+def test_id_as_positive(self):
+    """
+    Test for a positive Base Class id
+    """
+    base_instance = Base(100)
+    self.assertEqual(base_instance.id, 100)
+    base_instance = Base(10)
+    self.assertEqual(base_instance.id, 10)
