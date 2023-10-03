38d880d8df578c5cb9bc67ad208f28434099499c
matternstsr <matternstsr@gmail.com>
2023-10-03 11:00:33 -0500

    models/base.py:40:80: E501 line too long (81 > 79 characters)

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,15 +29,15 @@ class Base:
 
         with open(filename, mode='w', encoding='utf-8') as f:
             if list_objs is None:
-                    return f.write(cls.to_json_string(None))
+                return f.write(cls.to_json_string(None))
 
             else:
                 f.write(cls.to_json_string([obj.to_dictionary()
-                        for obj in list_objs]))
+                                            for obj in list_objs]))
 
     @staticmethod
     def from_json_string(json_string):
-        """Returns the list from the JSON string representation `json_string`."""
+        """Returns list from the JSON string representation `json_string`."""
         try:
             return json.loads(json_string)
         except (ValueError, TypeError):
