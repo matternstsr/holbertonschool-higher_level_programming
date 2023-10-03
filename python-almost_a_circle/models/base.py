8162d6558f5ad6a8f30dc4204d0943b3fd233bf9
matternstsr <matternstsr@gmail.com>
2023-10-03 12:36:51 -0500

    dont work but pushing anywys

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,6 +8,8 @@ import json
 class Base:
     """New Class Base"""
     __nb_objects = 0
+    FILE_EXTENSION = '.json'
+    ENCODING = 'utf-8'
 
     def __init__(self, id=None):
         """Initializes Base Class with id to assign to this obj"""
@@ -55,6 +57,41 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """Returns a list of instances loaded from a JSON file 'i think'."""
+        """This defines a class method named load_from_file. It's used to
+        load instances of the class from a JSON file and return them as a
+        list.."""
         file_name = cls.__name__ + cls.FILE_EXTENSION
+        """This line constructs the name of the JSON file by combining
+        the class name (cls.__name__) with the FILE_EXTENSION constant,
+        creating a filename like 'YourClassName.json.'"""
         instances = []
+        """This initializes an empty list called instances to
+        store the instances loaded from the JSON file."""
+        if path.isfile(file_name):
+            """This checks if the JSON file with the constructed file_name
+            exists in the current directory."""
+            with open(file_name, 'r', encoding=cls.ENCODING) as f:
+                """If the file exists, this line opens it in read mode ('r')
+                with the specified encoding (cls.ENCODING).
+                The file is opened within a context manager,
+                ensuring it's properly closed after use."""
+                dictionary = cls.from_json_string(f.read())
+                """This reads the contents of the JSON file (f.read())
+                and passes it to the from_json_string class method
+                to parse it into a dictionary.
+                The resulting dictionary represents the data
+                from the JSON file."""
+                for obj in dictionary:
+                    """This initiates a loop to iterate over each dictionary
+                    item in the dictionary. Each item represents data for an
+                    instance."""
+                    instances.append(cls.create(**obj))
+                    
+                    """Within the loop, this line creates an
+                    instance using the create class method,
+                    passing the unpacked obj
+                    (keyword arguments) to it.
+                    The created instance is then appended
+                    to the instances list."""
+        return instances
+
