0beee07ec008ae42b99f796fc7b2ae0e488c8757
matternstsr <matternstsr@gmail.com>
2023-10-03 14:15:13 -0500

    formatting?

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" A module that tests the Base class """
+"""
+A module that tests the Base class
+"""
 import unittest
 import pep8
 import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
-" I dont know what I need here!!!!!!!!!!!!"
 
 
 class TestBase(unittest.TestCase):
