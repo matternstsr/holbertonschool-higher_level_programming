33d71413fd09d16c83ffba0e19be297f706259d4
matternstsr <matternstsr@gmail.com>
2023-10-03 13:19:46 -0500

    added a class to teh tests. this si so weird

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,20 +5,22 @@ from models.rectangle import Rectangle
 from models.square import Square
 " I dont know what I need here!!!!!!!!!!!!"
 
-def test_id_as_negative(self):
-    """
-    Test for a negative Base Class id
-    """
-    base_instance = Base(-100)
-    self.assertEqual(base_instance.id, -100)
-    base_instance = Base(-10)
-    self.assertEqual(base_instance.id, -10)
 
-def test_id_as_positive(self):
-    """
-    Test for a positive Base Class id
-    """
-    base_instance = Base(100)
-    self.assertEqual(base_instance.id, 100)
-    base_instance = Base(10)
-    self.assertEqual(base_instance.id, 10)
+class TestBase(unittest.TestCase):
+    def test_id_as_negative(self):
+        """
+        Test for a negative Base Class id
+        """
+        base_instance = Base(-100)
+        self.assertEqual(base_instance.id, -100)
+        base_instance = Base(-10)
+        self.assertEqual(base_instance.id, -10)
+
+    def test_id_as_positive(self):
+        """
+        Test for a positive Base Class id
+        """
+        base_instance = Base(100)
+        self.assertEqual(base_instance.id, 100)
+        base_instance = Base(10)
+        self.assertEqual(base_instance.id, 10)
