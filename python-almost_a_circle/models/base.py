c144996a6f5f72b80856ee12c0f02811bf97d2bb
matternstsr <matternstsr@gmail.com>
2023-10-03 09:45:38 -0500

    ohhh cant be empty either

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,6 +18,6 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
