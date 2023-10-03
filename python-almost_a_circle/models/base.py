5009ea3352bd687ec103e3f5be674afa62723627
matternstsr <matternstsr@gmail.com>
2023-10-03 10:34:37 -0500

    ??

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,4 +29,9 @@ class Base:
 
         with open(filename, mode='w', encoding='utf-8') as f:
             if list_objs is None:
-                return f.write(cls.to_json_string(None))
+                for obj in list_objs:
+                    json_list.append(obj.to_dictionary())
+
+                    if len(json_list) > 0:
+                        json_string = Base.to_json_string(json_list)
+                        return f.write(cls.to_json_string(None))
