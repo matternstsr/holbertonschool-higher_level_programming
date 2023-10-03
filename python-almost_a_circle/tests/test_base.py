406cbf2852766a8372b25a6db02adc607564a742
matternstsr <matternstsr@gmail.com>
2023-10-03 17:02:51 -0500

    try that

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -4,8 +4,8 @@ A module that tests the Base class
 """
 import unittest
 from models.base import Base
-"from models.rectangle import Rectangle"
-"from models.square import Square"
+"""from models.rectangle import Rectangle"""
+"""from models.square import Square"""
 
 
 class TestBase(unittest.TestCase):
@@ -38,32 +38,16 @@ class TestBase(unittest.TestCase):
         self.assertTrue(isinstance(base_instance, Base))
 
     def test_string_id(self):
-        base_instance = Base('testing string id')
-        self.assertEqual(base_instance.id, 'testing string id')
-        base_instance = Base('we are in Python')
-        self.assertEqual(base_instance.id, 'we are in Python')
+        base_instance = Base('Monty Python')
+        self.assertEqual(base_instance.id, 'Monty Python')
+        base_instance = Base('Python is cool')
+        self.assertEqual(base_instance.id, 'Python is cool')
 
     def test_id_as_none(self):
         """
         Test for a None Base Class id
         """
         base_instance = Base()
-        self.assertEqual(base_instance.id, 10)
+        self.assertEqual(base_instance.id, 1)
         base_instance = Base(None)
-        self.assertEqual(base_instance.id, 20)
-
-    def test_empty_json_string(self):
-        """
-        Test for a empty data on the to_json_string method
-        """
-        empty = []
-        json_check = Base.to_json_string(empty)
-        self.assertEqual(json_check, "[]")
-
-        empty = None
-        json_check = Base.to_json_string(empty)
-        self.assertEqual(json_check, "[]")
-
-
-"""Getting the following tests"""
-"""0,2,22,25,36,48,52,83"""
+        self.assertEqual(base_instance.id, 2)
