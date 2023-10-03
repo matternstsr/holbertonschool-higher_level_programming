368bc059904d41f7011d1434f8fb7a408a4179d8
matternstsr <matternstsr@gmail.com>
2023-10-03 17:06:48 -0500

    pushing back

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -3,9 +3,8 @@
 A module that tests the Base class
 """
 import unittest
+import pep8
 from models.base import Base
-"""from models.rectangle import Rectangle"""
-"""from models.square import Square"""
 
 
 class TestBase(unittest.TestCase):
@@ -51,3 +50,12 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base_instance.id, 1)
         base_instance = Base(None)
         self.assertEqual(base_instance.id, 2)
+
+    def test_pep8_base(self):
+        """
+        Test that checks PEP8
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/base.py'])
+        self.assertEqual(check.total_errors, 0,
+                         "Found code style errors (and warnings).")
