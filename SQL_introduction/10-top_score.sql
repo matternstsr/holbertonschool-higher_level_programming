be5ac2a3b46a4e3c0a3298993889ebc4bfcf9748
matternstsr <matternstsr@gmail.com>
2023-10-20 00:59:59 -0500

    i dont see what is wrong

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,4 @@
--- all records of the table sort by score and display score and name
-SELECT score, name FROM hbtn_0c_0.second_table ORDER BY score DESC;
+-- all records of the table second_table of the database hbtn_0c_0
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+SELECT name, score FROM hbtn_0c_0.second_table ORDER BY score DESC;
