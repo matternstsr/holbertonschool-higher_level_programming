53a801f3465fb461dcd656a4011a86db865112e2
matternstsr <matternstsr@gmail.com>
2023-10-20 09:36:20 -0500

    Create if not there with permissions.

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Check if the user exists, and if not, create the user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Grant all privileges to the user on all databases
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
