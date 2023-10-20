1dc1c8d5aa616719980d8fb434785799b00c1838
matternstsr <matternstsr@gmail.com>
2023-10-20 00:05:55 -0500

    insert into table

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts new row in table first_table (database hbtn_0c_0) in server
-UPDATE first_table SET name = 'Best School' WHERE id = 89;
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
