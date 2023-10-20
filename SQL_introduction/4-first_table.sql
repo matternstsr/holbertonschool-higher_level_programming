e08702c6f4ef36d6bc9c5a8b35b979c78bcf7a1d
matternstsr <matternstsr@gmail.com>
2023-10-19 23:27:03 -0500

    seperated? it worked so dunno what it wants.

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,4 @@
 --creates table first_table in the current database MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256));
