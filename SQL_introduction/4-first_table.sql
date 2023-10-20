fc5ea043a1abacc948121f5082a2a2394ab11cdf
matternstsr <matternstsr@gmail.com>
2023-10-19 23:30:50 -0500

    im not sure

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
 --creates table first_table in the current database MySQL server.
-CREATE TABLE IF NOT EXISTS first_table(
-    id INT,
-    name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT,
+  name VARCHAR(256)
+);
