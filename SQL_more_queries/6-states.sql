03e34f54ea7f5620505d5920bad2bd2ec3c363c1
matternstsr <matternstsr@gmail.com>
2023-10-20 11:26:11 -0500

    have to "usu" ?

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- creates the database hbtn_0d_usa id INT unique, auto generated, can’t be null and is a primary key name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
