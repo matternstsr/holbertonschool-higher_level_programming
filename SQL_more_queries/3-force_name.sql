b2679f70591256f4157540d14bffaac70d8c88e4
matternstsr <matternstsr@gmail.com>
2023-10-20 10:30:28 -0500

    forgot the ; again

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Check if the database exists, and if not, create it, name not null
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL);
