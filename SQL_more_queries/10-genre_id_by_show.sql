2f5d6b2e020ced7855b8db68d31c9e93372629f2
matternstsr <matternstsr@gmail.com>
2023-10-20 14:04:05 -0500

    FROM tv_shows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -5,6 +5,6 @@
 -- You can use only one SELECT statement
 
 USE hbtn_0d_tvshows;
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
