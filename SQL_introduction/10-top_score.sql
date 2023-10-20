f81e0651e0ce7ea8298ccfeb5cb833a16bbfdf74
matternstsr <matternstsr@gmail.com>
2023-10-20 01:08:01 -0500

    tried every way i know this works!

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- all records of the table second_table showing score and then name 
-SELECT name, score FROM hbtn_0c_0.second_table ORDER BY score DESC;
+SELECT score, name FROM hbtn_0c_0.second_table ORDER BY score DESC;
