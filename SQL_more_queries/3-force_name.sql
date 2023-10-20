62cc4b1b03dfa6cf2b13c3d7281dc10ae6ea68bc
matternstsr <matternstsr@gmail.com>
2023-10-20 10:25:58 -0500

    ahh check is not null

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Check if the database exists, and if not, create it, name not null
-CREATE TABLE IF NOT EXISTS first_table(
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
-    name VARCHAR(256) WHERE IS NOT NULL);
+    name VARCHAR(256) CHECK (name IS NOT NULL)
