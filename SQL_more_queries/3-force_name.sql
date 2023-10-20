b2353688513ea7b37e9f3779b2e4889cd3712231
matternstsr <matternstsr@gmail.com>
2023-10-20 10:31:55 -0500

    force_name! what i get for copying old one instead of typing.

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
 -- Check if the database exists, and if not, create it, name not null
-CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256) NOT NULL);
+    name VARCHAR(256) NOT NULL
+);
