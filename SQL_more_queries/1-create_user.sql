46a21acb8821efcb35c5e999ee685867d7ec7ab8
matternstsr <matternstsr@gmail.com>
2023-10-20 09:47:48 -0500

    create and add permissions

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
 -- Check if the user exists, and if not, create the user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Grant all privileges to the user on all databases
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
--- Flush privileges to apply the changes
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
