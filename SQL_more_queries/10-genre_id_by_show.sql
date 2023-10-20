269db0fb5e2276a66076269b57c538779a596afa
matternstsr <matternstsr@gmail.com>
2023-10-20 14:37:18 -0500

    updated order

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,9 +4,9 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 
-USE hbtn_0d_tvshows;
-
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
