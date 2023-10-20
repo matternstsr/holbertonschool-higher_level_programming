1f7fd1f6d2fa5ad7959278872d49e5f15b3d8f03
matternstsr <matternstsr@gmail.com>
2023-10-20 12:07:15 -0500

    state not stat

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS cities (
     PRIMARY KEY (id),
     UNIQUE (id),
     name VARCHAR(256) NOT NULL,
-    stat_id INT NOT NULL,
+    state_id INT NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id));
