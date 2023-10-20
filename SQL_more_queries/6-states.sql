c21b44e90b72262b0c354d021a69c9406a97d91d
matternstsr <matternstsr@gmail.com>
2023-10-20 11:24:15 -0500

    switch them up a little

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa id INT unique, auto generated, can’t be null and is a primary key name VARCHAR(256) can’t be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    UNIQUE (id),
+    name VARCHAR(256) NOT NULL);
