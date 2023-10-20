3e464737f83fdb7f74904048e60665cd90dfee15
matternstsr <matternstsr@gmail.com>
2023-10-20 00:03:10 -0500

    set where id89

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,2 @@
 -- inserts new row in table first_table (database hbtn_0c_0) in server
-UPDATE TABLE first_table(
-    id 89,
-    name "Best School");
+UPDATE first_table SET name = 'Best School' WHERE id = 89;
