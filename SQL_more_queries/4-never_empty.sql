d7260de2b0c40b6da26ed4c7d0fa6c90e5cd551f
matternstsr <matternstsr@gmail.com>
2023-10-20 10:57:17 -0500

    I swear i tried this already but ok

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
--- Check if the database exists, and if not, create it, name not null
+-- creates the table id_not_null id INT with the default value 1
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT VALUES 1,
-    name VARCHAR(256));
+    id INT DEFAULT 1,
+    name VARCHAR(256)
