2aa1d0b76a254a0f0e5280b38cd236b71f058071
matternstsr <matternstsr@gmail.com>
2023-10-20 01:31:19 -0500

    helps if you update the right file time to stop for tonight

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,2 @@
--- lists all records with a score >= 10 in table second_table of database
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+-- all records of the table second_table showing score and then name 
+SELECT score, name FROM second_table ORDER BY score DESC;
