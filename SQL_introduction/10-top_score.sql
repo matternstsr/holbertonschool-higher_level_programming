e390e30fecaf3455e3c716144b186610ca2dfd6e
matternstsr <matternstsr@gmail.com>
2023-10-20 01:01:40 -0500

    comments still?

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,2 @@
--- all records of the table second_table of the database hbtn_0c_0
--- Results should display both the score and the name (in this order)
--- Records should be ordered by score (top first)
+-- all records of the table second_table showing score and then name 
 SELECT name, score FROM hbtn_0c_0.second_table ORDER BY score DESC;
