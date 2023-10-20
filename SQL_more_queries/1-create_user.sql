2be193e7f61e9fe925195cd50a3696844dd56cc2
matternstsr <matternstsr@gmail.com>
2023-10-20 09:41:33 -0500

    need to flush (set permissions or apply)

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,3 +2,5 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to the user on all databases
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
