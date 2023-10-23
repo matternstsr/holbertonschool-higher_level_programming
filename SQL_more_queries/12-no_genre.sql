0832dbdee2df4d140d974cd376b230b3189b15ad
matternstsr <matternstsr@gmail.com>
2023-10-23 10:05:33 -0500

    tv_shows.title - tv_show_genres.genre_id

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -8,5 +8,5 @@
 
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_shows.title - tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
