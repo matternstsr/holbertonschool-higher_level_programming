c989c87d1eec130edff50439bff44e55e1dc7d3c
matternstsr <matternstsr@gmail.com>
2023-10-23 12:03:21 -0500

    dont need both Left joins duh.

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -5,6 +5,5 @@
 -- You can use only one SELECT statement
 SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre_name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_shows.id = tv_genres.show_id
 ORDER BY tv_shows.title ASC, genre_name ASC;
