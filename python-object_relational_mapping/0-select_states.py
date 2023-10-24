aeaf1b4479b491a7580c1414eef646611f3296c7
matternstsr <matternstsr@gmail.com>
2023-10-24 08:56:56 -0500

    flake8 issues

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""SelectStates module"""
-"""Write a script that lists all states from the database hbtn_0e_0_usa
-Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+"""SelectStates module
+Write a script that lists all states from the database hbtn_0e_0_usa
+Your script should take 3 arguments: mysql username, mysql password and
+database name (no argument validation needed)
 You must use the module MySQLdb (import MySQLdb)
-Your script should connect to a MySQL server running on localhost at port 3306
+Your script should connect to a MySQL server running on localhost at
+port 3306
 Results must be sorted in ascending order by states.id
 Results must be displayed as they are in the example below
 Your code should not be executed when imported"""
@@ -24,13 +26,14 @@ def select_states():
                          user=username,
                          passwd=password,
                          db=database)
-    database_cursor  = db.cursor()
+    database_cursor = db.cursor()
     database_cursor.execute('SELECT * FROM states ORDER BY id ASC')
     states = database_cursor.fetchall()
     for state in states:
         print(state)
     database_cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     select_states()
