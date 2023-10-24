77ed97e0e2b440cfe6767e2803f795156e157ae1
matternstsr <matternstsr@gmail.com>
2023-10-24 11:01:07 -0500

    lots of flake8 grrr

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,12 +4,13 @@ from the database hbtn_0e_6_usa.
 Your script should take 3 arguments: mysql username, mysql password and
 database name. You must use the module SQLAlchemy
 You must import State and Base from model_state - from model_state import
-Base, State. Your script should connect to a MySQL server running on localhost at port 3306
+Base, State. Your script should connect to a MySQL server running on
+localhost at port 3306.
 The state you display must be the first in states.id
-You are not allowed to fetch all states from the database before 
-displaying the result. The results must be displayed as they are in the example below
-If the table states is empty, print Nothing followed by a new line
-Your code should not be executed when imported"""
+You are not allowed to fetch all states from the database before
+displaying the result. The results must be displayed as they are in
+the example below. If the table states is empty, print Nothing followed
+by a new line. Your code should not be executed when imported"""
 
 import sys
 from sqlalchemy.orm import sessionmaker
@@ -36,6 +37,7 @@ def fetch_first():
     else:
         print("Nothing")
     session.close()
+
 
 if __name__ == "__main__":
     fetch_first()
