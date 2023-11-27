b25200866b332b3ee4e65aa9a679b4583def1b48
matternstsr <matternstsr@gmail.com>
2023-11-27 13:40:11 -0600

    this one sucks

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined || process.argv[3] === undefined) { console.log('HBTN is undefined'); } else { const secondArg = process.argv[3] === 'undefined' ? 'undefined' : process.argv[3]; console.log(`${process.argv[2]} is ${secondArg}`); }
+if (process.argv[2] === undefined) { console.log('undefined is undefined'); } else { const secondArg = process.argv[3] === undefined ? 'undefined' : process.argv[3]; console.log(`${process.argv[2]} is ${secondArg}`); }
