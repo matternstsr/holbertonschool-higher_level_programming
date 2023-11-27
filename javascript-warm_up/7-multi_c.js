673e7177de10c03876e35240e8b5162e8c1a95b1
matternstsr <matternstsr@gmail.com>
2023-11-27 14:20:13 -0600

    used semistanadard to fix issues again

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ const times = parseInt(process.argv[2]);
 
 if (!isNaN(times) && Number.isInteger(times)) {
   for (let i = 0; i < times; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 } else {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 }
