3664be4eded83d0a360c1937a905ed4d418e8592
matternstsr <matternstsr@gmail.com>
2023-11-27 13:58:00 -0600

    i see it JavaScript is amazing not Javascript is amazing

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const lines = ['C is fun', 'Python is Cool', 'Javascript is amazing'];
+const lines = ['C is fun', 'Python is Cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
