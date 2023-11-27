26060dbc73728ce043e06bbb3f9065e41583860c
matternstsr <matternstsr@gmail.com>
2023-11-27 15:26:54 -0600

    trying a differnt way

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 
-const factorial = (n) => {
-  if (isNaN(n)) { return 1; }
-
-  if (n === 0) { return 1; }
-
-  return n * factorial(n - 1);
-};
+function factorial (a) {
+  if (a < 0) { return 'Undefined'; } else if (isNaN(a) || a === 0 || a === 1) { return 1; } else { return a * factorial(a - 1); }
+}
 
 const arg = process.argv[2];
-
 const n = parseInt(arg);
-
 const result = factorial(n);
 
-console.log(`Factorial of ${arg} is: ${result}`);
+console.log(result.toString());
