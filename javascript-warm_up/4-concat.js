7017b8b9367a14027f305beec67c3b30370b4a99
matternstsr <matternstsr@gmail.com>
2023-11-27 13:37:02 -0600

    putting HBTN is undefined in there

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined || process.argv[3] === undefined) { console.log('Please provide two arguments'); } else { const secondArg = process.argv[3] === 'undefined' ? 'undefined' : process.argv[3]; console.log(`${process.argv[2]} is ${secondArg}`); }
+if (process.argv[2] === undefined || process.argv[3] === undefined) { console.log('HBTN is undefined'); } else { const secondArg = process.argv[3] === 'undefined' ? 'undefined' : process.argv[3]; console.log(`${process.argv[2]} is ${secondArg}`); }
