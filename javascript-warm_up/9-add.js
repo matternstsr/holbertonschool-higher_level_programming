775d88b581fa805d603c8ab316b227a832b39dc1
matternstsr <matternstsr@gmail.com>
2023-11-27 14:37:53 -0600

    trying something else

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -6,4 +6,4 @@ const intone = parseInt(process.argv[2]);
 const inttwo = parseInt(process.argv[3]);
 
 if (!isNaN(intone) && Number.isInteger(intone) &&
-    !isNaN(inttwo) && Number.isInteger(inttwo)) { console.log(add(intone, inttwo)); }
+    !isNaN(inttwo) && Number.isInteger(inttwo)) { console.log(add(intone, inttwo)); } else { const sumoftwo = intone + inttwo; console.log(sumoftwo); }
