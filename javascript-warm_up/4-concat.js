5671326c0abc4f28818287cee5d6d0328747397f
matternstsr <matternstsr@gmail.com>
2023-11-27 13:30:51 -0600

    not sure this is what it wants but good test

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv[2] === undefined || process.argv[3] === undefined) {
+  console.log('Please provide two arguments');
+} else {
   console.log(`${process.argv[2]} is ${process.argv[3]}`);
 }
