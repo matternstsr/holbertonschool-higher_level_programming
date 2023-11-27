d92d9499f486110c3b03de77bff9ae10ebe3b3f9
matternstsr <matternstsr@gmail.com>
2023-11-27 13:35:55 -0600

    see what that does

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  console.log('Please provide two arguments');
-} else {
-  console.log(`${process.argv[2]} is ${process.argv[3]}`);
-}
+if (process.argv[2] === undefined || process.argv[3] === undefined) { console.log('Please provide two arguments'); } else { const secondArg = process.argv[3] === 'undefined' ? 'undefined' : process.argv[3]; console.log(`${process.argv[2]} is ${secondArg}`); }
