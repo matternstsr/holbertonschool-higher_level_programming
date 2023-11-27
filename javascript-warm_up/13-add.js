96769872008aa533002844a4adb35cca553cced8
matternstsr <matternstsr@gmail.com>
2023-11-27 15:49:32 -0600

    easy function but module.exports ... what!!!!

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function add(a, b) {
-    return a + b;
-  }
-  module.exports = { add };
-// In Node.js, the module.exports object is a special object that is included 
-// in every JavaScript file in the Node.js application. It is used to define 
+function add (a, b) {
+  return a + b;
+}
+module.exports = { add };
+// In Node.js, the module.exports object is a special object that is included
+// in every JavaScript file in the Node.js application. It is used to define
 // what a module exports and makes available for other modules to require!!!
