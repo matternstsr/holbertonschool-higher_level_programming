50335e13ca7d0a372a871ec8a97caf2909302d77
matternstsr <matternstsr@gmail.com>
2023-11-27 14:19:05 -0600

    will i ever get the caps right lol

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ const times = parseInt(process.argv[2]);
 
 if (!isNaN(times) && Number.isInteger(times)) {
   for (let i = 0; i < times; i++) {
-    console.log("c is fun");
+    console.log("C is fun");
   }
 } else {
-  console.log("missing number of occurrences");
+  console.log("Missing number of occurrences");
 }
