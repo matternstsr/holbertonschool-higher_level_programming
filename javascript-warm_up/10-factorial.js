5f83dcb54ca22b50c00f0a04e019945c54448e89
matternstsr <matternstsr@gmail.com>
2023-11-27 14:44:51 -0600

    10-factorial.js:17:40: 'reslt' is not defined. (no-undef)

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const factorial = (n) => {
+  if (isNaN(n)) { return 1; }
+
+  if (n === 0) { return 1; }
+
+  return n * factorial(n - 1);
+};
+
+const arg = process.argv[2];
+
+const n = parseInt(arg);
+
+const result = factorial(n);
+
+console.log(`Factorial of ${arg} is: ${result}`);
