ac0fcc21c1412fee225a4d5069d4056d4fbdaabe
matternstsr <matternstsr@gmail.com>
2023-11-27 13:06:40 -0600

    syntax is so wierd on this

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,11 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const numArgs = process.argv.length - 2;
-if (numArgs == 0) {console.log("No argument");}
-else if (numArgs == 1) {console.log("Argument found");}
-else {console.log("Arguments found");}
+
+if (numArgs === 0) {
+console.log("No argument");
+} else if (numArgs === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
