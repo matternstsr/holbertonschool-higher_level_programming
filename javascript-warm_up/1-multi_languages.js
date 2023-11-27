ca7501e2284ab2277678af302ef2319f84508f7a
matternstsr <matternstsr@gmail.com>
2023-11-27 12:56:46 -0600

    123

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// Print the first line
+const firstOne = 'C is fun';
+console.log(firstOne);
+
+// Print the second line
+const secondOne = 'Python is cool';
+console.log(secondOne);
+
+// Print the third line
+const thirdOne = 'JavaScript is amazing';
+console.log(thirdOne);
