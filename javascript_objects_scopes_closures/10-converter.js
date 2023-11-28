23d8e77619625d67bcf8365674f763811f3118dc
matternstsr <matternstsr@gmail.com>
2023-11-28 17:22:14 -0600

    tried logical OR (||) to provide an empty string when undefined

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-    return process.argv[2].toString(base); };
+exports.converter = function (base) { return (process.argv[2] || '').toString(base); };
+// handled the case where undefined
+// tried logical OR (||) to provide an empty string when undefined
