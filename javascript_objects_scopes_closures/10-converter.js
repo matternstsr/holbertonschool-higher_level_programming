88ed2442df3c8e7c6d5fda2a0b99c0190e347bd1
matternstsr <matternstsr@gmail.com>
2023-11-28 17:30:40 -0600

    used num as the passed var instead of using argv.

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-exports.converter = function (base) { return (process.argv[2] || '').toString(base); };
+exports.converter = function (base) { return num => num.toString(base); };
 // handled the case where undefined
 // tried logical OR (||) to provide an empty string when undefined
+// used num as the passed var instead of using argv.
