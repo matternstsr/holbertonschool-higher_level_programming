b653cc7014cab01554e29c866583e105cf4f85c2
matternstsr <matternstsr@gmail.com>
2023-11-28 15:48:59 -0600

    6-square.js:4:3: Useless constructor.  removed   constructor (size) { super(size); }

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const OldSquare = require('./5-square');
 class Square extends OldSquare {
-  constructor (size) { super(size); }
-
   charPrint (c) {
     const whichPrint = c || 'X';
     for (let i = 0; i < this.height; i++) { console.log(whichPrint.repeat(this.width)); }
