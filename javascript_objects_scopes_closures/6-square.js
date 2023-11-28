bd91e39b63e348f9fd6c83ee0409a4919f95df93
matternstsr <matternstsr@gmail.com>
2023-11-28 15:51:30 -0600

    added notes why i removed the constructor.. not totally getting how it uses the other constructor but it says that it does and that the new conctructor is usless.

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -7,3 +7,7 @@ class Square extends OldSquare {
   }
 }
 module.exports = Square;
+// the constructor in the Square class is considered useless.
+// The reason for this suggestion might be that the constructor in Square is doing the same
+// thing as the constructor in the parent class, and it doesn't have any additional functionality.
+// omit the constructor in the Square class if it doesn't have additional logic.
