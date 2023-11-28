2c5fee4c7a812131d9d916ab2c4bdd071f70ae63
matternstsr <matternstsr@gmail.com>
2023-11-28 15:44:34 -0600

    6 TRY 1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,11 @@
+#!/usr/bin/node
+const OldSquare = require('./5-square');
+class Square extends OldSquare {
+  constructor (size) { super(size); }
 
+  charPrint (c) {
+    const whichPrint = c || 'X';
+    for (let i = 0; i < this.height; i++) { console.log(whichprint.repeat(this.width)); }
+  }
+}
+module.exports = Square;
