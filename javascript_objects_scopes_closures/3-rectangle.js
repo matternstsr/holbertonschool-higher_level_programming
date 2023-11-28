fcc068668f84a3ffbe1aaad156c5e44a579fd54f
matternstsr <matternstsr@gmail.com>
2023-11-28 14:49:59 -0600

    not sure how this repeat is supposed to work...

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {if (w > 0 && h > 0) {this.width = w; this.height = h;} else {Object.create(null);}}
+    print() {if (this.width && this.height) {for (let i = 0; i < this.height; i++) {console.log('X'.repeat(this.width));}}}}  
+  module.exports = Rectangle;
