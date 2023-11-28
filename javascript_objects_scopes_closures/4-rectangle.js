62fd720e567a98d752b3acd28eb44ea9b2a6d5a2
matternstsr <matternstsr@gmail.com>
2023-11-28 15:09:31 -0600

    for got to semistandard --fix 4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w > 0 && h > 0) {this.width = w;this.height = h;}
-       else {Object.create(null);}}
+  constructor (w, h) {
+    if (w > 0 && h > 0) { this.width = w; this.height = h; } else { Object.create(null); }
+  }
 
-    print() {
-      if (this.width && this.height) {
-        for (let i = 0; i < this.height; i++) {console.log('X'.repeat(this.width));}}}
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
+    }
+  }
 
-    rotate() {[this.width, this.height] = [this.height, this.width];}
-    dooble() {this.width *= 2;this.height *= 2;}}  
-  module.exports = Rectangle;
+  rotate () { [this.width, this.height] = [this.height, this.width]; }
+  dooble () { this.width *= 2; this.height *= 2; }
+}
+module.exports = Rectangle;
