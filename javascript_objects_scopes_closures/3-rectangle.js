761404ef747d0065e88e4e08603156bd1fa4dd47
matternstsr <matternstsr@gmail.com>
2023-11-28 14:51:10 -0600

    after semistandard

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {if (w > 0 && h > 0) {this.width = w; this.height = h;} else {Object.create(null);}}
-    print() {if (this.width && this.height) {for (let i = 0; i < this.height; i++) {console.log('X'.repeat(this.width));}}}}  
-  module.exports = Rectangle;
+  constructor (w, h) { if (w > 0 && h > 0) { this.width = w; this.height = h; } else { Object.create(null); } }
+  print () { if (this.width && this.height) { for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); } } }
+}
+module.exports = Rectangle;
