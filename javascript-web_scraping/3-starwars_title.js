c0f78d9385e47bbbf4f7c45edca078f2076277b7
matternstsr <matternstsr@gmail.com>
2023-12-04 16:47:16 -0600

    fixed the const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -6,15 +6,15 @@ const movieId = process.argv[2];
 if (!movieId) { console.error('Usage: ./3-starwars_title.js <movie-id>'); process.exit(1); }
 
 // Construct the URL for the Star Wars API
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/{movieId}';
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 // Make a GET request to the Star Wars API
 request.get(apiUrl, (error, response, body) => {
   if (error) { console.error(error); process.exit(1); }
 
-// Parse the JSON response
+  // Parse the JSON response
   const movieData = JSON.parse(body);
 
-// Check if the movie data is valid
+  // Check if the movie data is valid
   if (response.statusCode === 200 && movieData.title) { console.log(movieData.title); } else { console.error('Movie not found.'); }
 });
