2433d0965cb29e07ccfe68918acdb4ffc7dd23ac
matternstsr <matternstsr@gmail.com>
2023-12-04 16:25:30 -0600

    trying two again with a different display

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,23 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
 
-// Get file path and string to write from command line arguments
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
+// Checking if the URL is provided
+if (!url) { console.error('Usage: ./2-statuscode.js <URL>'); process.exit(1); }
 
-// Check if both file path and content are provided
-if (!filePath || !contentToWrite) {
-  console.error('Usage: node 1-writeme.js <file-path> <content-to-write>');
-  process.exit(1);
-}
+// Making a get request to the specified URL
+request.get(url, (error, response) => { if (error) { console.error(error); process.exit(1); }
 
-// Write the string to the file in utf-8 encoding
-fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
-  // Check for errors during writing
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  console.log(`[Got]\nContent has been written to ${filePath}`);
-});
+  // Display the status code
+  console.log('code:', response.statusCode); });
