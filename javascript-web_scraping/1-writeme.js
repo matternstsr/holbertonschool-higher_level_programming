a368737b6e38cfa6974b9c7f7b1c82cd23cd8cd1
matternstsr <matternstsr@gmail.com>
2023-12-04 15:59:08 -0600

    trying 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+// Check if a URL
+if (!url) { console.error('Usage: node 2-statuscode.js <URL>'); process.exit(1);}
+
+// Make a request and check for errors??
+request(url, (error, response) => { if (error) { console.error(`Error: {error.message}`); process.exit(1);}
+
+  // Print the status code to the answer
+  console.log(`code: {response.statusCode}`);});
