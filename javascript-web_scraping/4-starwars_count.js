cf7b1549f7cbcb1d93099930dcc1d80d8dd73cf5
matternstsr <matternstsr@gmail.com>
2023-12-04 17:18:02 -0600

    ok now 4 should work

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,21 +5,27 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 // Checking if the API URL is provided
-if (!apiUrl) { console.error('Usage: ./4-starwars_count.js <api-url>'); process.exit(1); }
+if (!apiUrl) {
+  console.error('Usage: ./4-starwars_count.js <api-url>');
+  process.exit(1);
+}
 
-// Setting char ID for Wedge Antilles
+// Setting the char for ID for Wedge Antilles
 const wedgeAntillesId = 18;
 
 // Making a GET request to the Star Wars API
 request.get(apiUrl, (error, response, body) => {
-  if (error) { console.error(error); process.exit(1); }
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
 
   // Parsing the JSON response
   const filmsData = JSON.parse(body);
 
   // Filtering movies where Wedge Antilles
   const moviesWithWedge = filmsData.results.filter((film) => {
-    return film.characters.includes('https://swapi-api.hbtn.io/api/people/{wedgeAntillesId}/');
+    return film.characters.includes(`https://swapi-api.hbtn.io/api/people/${wedgeAntillesId}/`);
   });
 
   // Display the count of movies with Wedge Antilles
--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node#
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+// Geting the URL and file path from the command line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Checking if both arguments are provided
+if (!url || !filePath) { console.error('Usage: ./5-request_store.js <URL> <file-path>'); process.exit(1); }
+
+// Making a GET request to the specified URL
+request.get(url, (error, response, body) => {
+  if (error) { console.error(error); process.exit(1); }
+
+  // Writing the contents to the specified file
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) { console.error(err); process.exit(1); }
+
+    console.log('body response written to {filePath}');
+  });
+});
