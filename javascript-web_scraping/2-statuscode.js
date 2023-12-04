9f815917b0cd063206761215410f19f7f34703a1
matternstsr <matternstsr@gmail.com>
2023-12-04 16:26:25 -0600

    emi standard and i dont see eye to eye

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -6,7 +6,9 @@ const url = process.argv[2];
 if (!url) { console.error('Usage: ./2-statuscode.js <URL>'); process.exit(1); }
 
 // Making a get request to the specified URL
-request.get(url, (error, response) => { if (error) { console.error(error); process.exit(1); }
+request.get(url, (error, response) => {
+  if (error) { console.error(error); process.exit(1); }
 
   // Display the status code
-  console.log('code:', response.statusCode); });
+  console.log('code:', response.statusCode);
+});
