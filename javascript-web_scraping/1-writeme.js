72dbee1281e0f0449b9d3f9d3cfceb1214a9ea18
matternstsr <matternstsr@gmail.com>
2023-12-04 16:01:29 -0600

    trying that again

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,14 @@
-const request = require('request');
-const url = process.argv[2];
+#!/usr/bin/node
+const fs = require('fs');
 
-// Check if a URL
-if (!url) { console.error('Usage: node 2-statuscode.js <URL>'); process.exit(1);}
+// Get file path and string to write from command line arguments
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
 
-// Make a request and check for errors??
-request(url, (error, response) => { if (error) { console.error(`Error: {error.message}`); process.exit(1);}
+// Check if both file path and content are provided
+if (!filePath || !contentToWrite) { console.error('Usage: node 1-writeme.js <file-path> <content-to-write>'); process.exit(1); }
 
-  // Print the status code to the answer
-  console.log(`code: {response.statusCode}`);});
+// Write the string to the file in utf-8 encoding
+fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => { if (error) { console.error(error); process.exit(1); }
+
+  console.log(`Content has been written to ${filePath}`); });
