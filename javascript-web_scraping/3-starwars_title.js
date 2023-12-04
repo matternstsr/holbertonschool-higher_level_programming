940ba880809832ca8c17ae7f2b7e39182ba28b76
matternstsr <matternstsr@gmail.com>
2023-12-04 16:41:53 -0600

    not right but need help so pushing

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+
+// Check if the movie ID is provided
+if (!movieId) { console.error('Usage: ./3-starwars_title.js <movie-id>'); process.exit(1); }
+
+// Construct the URL for the Star Wars API
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/{movieId}';
+
+// Make a GET request to the Star Wars API
+request.get(apiUrl, (error, response, body) => {
+  if (error) { console.error(error); process.exit(1); }
+
+// Parse the JSON response
+  const movieData = JSON.parse(body);
+
+// Check if the movie data is valid
+  if (response.statusCode === 200 && movieData.title) { console.log(movieData.title); } else { console.error('Movie not found.'); }
+});
