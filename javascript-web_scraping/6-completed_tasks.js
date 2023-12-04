30ee292e9ba68b0e4d367aff5ac15cb2ca68863d
matternstsr <matternstsr@gmail.com>
2023-12-04 17:31:32 -0600

    I hate how it moves my comments with semi standard on 6. Fixed 4 i think as well

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+// Checking if the API URL is provided
+if (!apiUrl) {
+  console.error('Usage: ./6-completed_tasks.js <api-url>'); process.exit(1);
+}
+
+// Making a GET request to the specified API URL
+request.get(apiUrl, (error, response, body) => {
+  if (error) { console.error(error); process.exit(1); }
+
+  // Parsing the JSON response
+  const todos = JSON.parse(body);
+
+  // Creating an object to store the count of completed tasks per user
+  const completedTasksByUser = {};
+
+  // Going through the todo's and count completedtasksbyuser
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedTasksByUser[todo.userId]) { completedTasksByUser[todo.userId]++; } else { completedTasksByUser[todo.userId] = 1; }
+    }
+  });
+
+  // Print the results
+  console.log(completedTasksByUser);
+});
