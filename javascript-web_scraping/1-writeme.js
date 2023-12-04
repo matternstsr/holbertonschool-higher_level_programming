c03a57297fa8527de966182e1f5156be8ac0bdbf
matternstsr <matternstsr@gmail.com>
2023-12-04 16:12:57 -0600

    Content has been written to w_file_0 was not welcome lol

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-// Get file path and string to write from command line arguments
+// Get file path and string from command line arguments
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
-// Check if both file path and content are provided
-if (!filePath || !contentToWrite) { console.error('Usage: node 1-writeme.js <file-path> <content-to-write>'); process.exit(1); }
+// Check if both arguments are provided
+if (!filePath || !contentToWrite) {
+  console.error('Usage: node 1-writeme.js <file-path> <string-to-write>');
+  process.exit(1);
+}
 
-// Write the string to the file in utf-8 encoding
-fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  console.log(`Content has been written to ${filePath}`);
+// Write content to the file
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) { console.error(err); }
 });
--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+// Get file path and string to write from command line arguments
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+// Check if both file path and content are provided
+if (!filePath || !contentToWrite) {
+  console.error('Usage: node 1-writeme.js <file-path> <content-to-write>');
+  process.exit(1);
+}
+
+// Write the string to the file in utf-8 encoding
+fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
+  // Check for errors during writing
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  console.log(`[Got]\nContent has been written to ${filePath}`);
+});
