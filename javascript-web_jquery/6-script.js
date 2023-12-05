e87b0701a8cd191bed7599cb72d8436e1771738a
matternstsr <matternstsr@gmail.com>
2023-12-05 13:42:57 -0600

    changing text instead of color interesting.. like it

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,14 @@
+// 6-script.js
+
+// Wait for the DOM load
+$(document).ready(function () {
+    // Using the jQuery API by selecting toogle header
+    var updateHeaderDiv = $("#update_header");
+  
+    // use the click event handler
+    updateHeaderDiv.click(function () {
+      // update the text for the chosen header
+      $("header").text("New Header!!!!");
+    });
+  });
+  
