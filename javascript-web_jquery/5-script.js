584dcaffc26ad973ec5e3279681841c922a97531
matternstsr <matternstsr@gmail.com>
2023-12-05 15:32:07 -0600

    Change the ('<LI>Item</LI>') cause it specifically asked for LI

--- javascript-web_jquery/5-script.js
+++ javascript-web_jquery/5-script.js
@@ -4,6 +4,6 @@ $(document).ready(function () {
   // use the click event handler
   $('DIV#add_item').click(function () {
     // put li into ul using the class of "my_list"
-    $('UL.my_list').append('<li>Item</li>');
+    $('UL.my_list').append('<LI>Item</LI>');
   });
 });
