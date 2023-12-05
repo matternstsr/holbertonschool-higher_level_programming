c1ee746045fa1f18be8b9efd1b81c3e858bbf398
matternstsr <matternstsr@gmail.com>
2023-12-05 13:11:29 -0600

    not psure how to use the semistd that they want with the flags but moving on.

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+
+// Wait for the DOM load
+$(document).ready(function () {
+    // Using the jQuery API by selecting red header
+    var redHeaderDiv = $("#red_header");
+  
+    // use the click event handler
+    redHeaderDiv.click(function () {
+      // add the class "red" to i the header that is chosen
+      $("header").addClass("red");
+    });
+  });
+  
