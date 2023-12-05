fe65d5ffd35ea286393100b3a4612acdc08d3b94
matternstsr <matternstsr@gmail.com>
2023-12-05 15:15:57 -0600

    fixed the HEADER

--- javascript-web_jquery/1-script.js
+++ javascript-web_jquery/1-script.js
@@ -3,5 +3,5 @@
 // Wait for the DOM load
 $(document).ready(function () {
   // Update the text color to red (#FF0000)
-  $('header').css('color', '#FF0000');
+  $('HEADER').css('color', '#FF0000');
 });
