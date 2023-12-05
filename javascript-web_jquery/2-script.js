5f76d1a3495f2fe27e92dd3105368082df3c191c
matternstsr <matternstsr@gmail.com>
2023-12-05 15:15:17 -0600

    fixed the 'DIV#red_header' and the HEADER as req glad i checked all this

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -3,8 +3,8 @@
 // Wait for the DOM load
 $(document).ready(function () {
   // use the click event handler
-  $('#red_header').click(function () {
+  $('DIV#red_header').click(function () {
     // Update the text color to red
-    $('header').css('color', '#FF0000');
+    $('HEADER').css('color', '#FF0000');
   });
 });
