b3217bbc1719c1ca186a48086d7839b5170d2763
matternstsr <matternstsr@gmail.com>
2023-12-05 12:47:29 -0600

    #FF0000 not #FFF000 that is yellow come to find out

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -6,6 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var headerElement = document.querySelector("header");
   
     // Update the text color to red (#FF0000)
-    headerElement.style.color = "#FFF000";
+    headerElement.style.color = "#FF0000";
   });
   
