eeff69e02fdeca5d00c2804914842c54bd3ccbce
matternstsr <matternstsr@gmail.com>
2023-12-05 15:12:24 -0600

    fixed the 'DIV#toggle_header' as req

--- javascript-web_jquery/4-script.js
+++ javascript-web_jquery/4-script.js
@@ -3,7 +3,7 @@
 // Wait for the DOM content to be fully loaded
 $(document).ready(function () {
   // use the click event handler
-  $('#toggle_header').click(function () {
+  $('DIV#toggle_header').click(function () {
     // add a toggle the class between red and green
     $('header').toggleClass('red green');
   });
