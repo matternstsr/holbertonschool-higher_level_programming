a33fcbc838679afc9b1a6fd0d8f2adb214e88555
matternstsr <matternstsr@gmail.com>
2023-12-05 12:56:54 -0600

    forgot the $ for the jquery

--- javascript-web_jquery/1-script.js
+++ javascript-web_jquery/1-script.js
@@ -3,7 +3,7 @@
 // Wait for the DOM load
 $(document).ready(function () {
     // Select the headerelement using the jQuery API
-    var headerElement = ("header");
+    var headerElement = $("header");
   
     // Update the text color to red (#FF0000)
     headerElement.css("color", "#FF0000");
