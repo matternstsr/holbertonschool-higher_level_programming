26d3fbb1c9ab202a6f42105d1b4e22ad1c42e245
matternstsr <matternstsr@gmail.com>
2023-12-05 15:17:12 -0600

    not sure that 0- 2 needed the HEADER but felt right

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -3,5 +3,5 @@
 // Wait for the DOM load
 document.addEventListener('DOMContentLoaded', function () {
   // Update the text color to red (#FF0000)
-  document.querySelector('header').style.color = '#FF0000';
+  document.querySelector('HEADER').style.color = '#FF0000';
 });
